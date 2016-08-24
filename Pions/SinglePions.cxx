0068e3d419fe73b558a3a9dabc1f5cd4c0e43b0c
Ann Laube <laube@pplxint8.physics.ox.ac.uk>
2016-08-24 15:11:06 +0100

    Clean up

--- Pions/SinglePions.cxx
+++ Pions/SinglePions.cxx
@@ -18,131 +18,151 @@ namespace larlite {
 // ---------------------------------------------------  
   bool SinglePions::analyze(storage_manager* storage) {
   
-   // Event-by-Event analysis
+    // Event-by-Event analysis
 
-  auto mc = storage->get_data<event_mctruth>("generator");
-  auto mctrk_v = storage->get_data<larlite::event_mctrack>("mcreco");  	// this is a vector of MCtracks
-  auto mcshow_v = storage->get_data<event_mcshower>("mcreco");      		// this is a vector of MCshowers
-  auto mcparticles = mc->at(0).GetParticles(); 
-  auto pfp_v = storage->get_data<event_pfpart>("pandoraCosmic");			// PandoraPFParticles
+    auto mc = storage->get_data<event_mctruth>("generator");
+    auto mctrk_v = storage->get_data<larlite::event_mctrack>("mcreco");  	// this is a vector of MCtracks
+    auto mcshow_v = storage->get_data<event_mcshower>("mcreco");      		// this is a vector of MCshowers
+    auto mcparticles = mc->at(0).GetParticles(); 
+    auto pfp_v = storage->get_data<event_pfpart>("pandoraCosmic");			// PandoraPFParticles
 
-  event_vertex* ev_vertex = nullptr;
-  auto const& ass_vertex = storage->find_one_ass(pfp_v->id(), ev_vertex, "pandoraCosmic");
+    event_vertex* ev_vertex = nullptr;
+    auto const& ass_vertex = storage->find_one_ass(pfp_v->id(), ev_vertex, "pandoraCosmic");
 
-  event_track* ev_track = nullptr;
-  auto const& ass_tracks = storage->find_one_ass(pfp_v->id(), ev_track, "pandoraCosmicKHit");	// vector of track indices associated with the pfparticle...
+    event_track* ev_track = nullptr;
+    auto const& ass_tracks = storage->find_one_ass(pfp_v->id(), ev_track, "pandoraCosmicKHit");	// vector of track indices associated with the pfparticle...
   
-  // interaction mode 
-  bool decayPion = false;
-  bool piInelasticPion = false;
-  bool exitPion = false;
+    // interaction mode 
+    bool decayPion = false;
+    bool piInelasticPion = false;
+    bool exitPion = false;
 
 
-  //other variables
-  int countDaughters = 0;
-  double borderdist [6];
+    //other variables
+    int countDaughters = 0;
+    double borderdist [6];
+    std::string pionProcess = "UNKNOWN";
 
-  std::cout << "\n----------------------------------------" << std::endl;
-  std::cout << "Run   " << storage->run_id() << "  Event " << storage->event_id() << std::endl << std::endl; 
-  for(auto par : mcparticles) {
-    std::cout << "MCPART " << par.StatusCode() << "\t PDG : " << par.PdgCode() <<  std::endl;
-    mcpart++;
-    if (par.PdgCode() == 211) {
-      auto pion = par;
-      PionE = pion.Momentum().E();
-      PionEnergy->Fill(PionE);
-      PionP.SetXYZ(pion.Momentum().Px(), pion.Momentum().Py(), pion.Momentum().Pz());
-      PionMom = PionP.Mag();
-      PionMomentum->Fill(PionMom);
-    }
-  } 
-
-  if(mctrk_v->size() != 0) {
+    for(auto par : mcparticles) {
+      mcpart++;
+      if (par.PdgCode() == 211) {
+        auto pion = par;
+        PionE = pion.Momentum().E();
+        PionEnergy->Fill(PionE);
+        PionP.SetXYZ(pion.Momentum().Px(), pion.Momentum().Py(), pion.Momentum().Pz());
+        PionMom = PionP.Mag();
+        PionMomentum->Fill(PionMom);
+      }
+    } 
   
-    for(auto mctrk : *mctrk_v)  {
-   
-      if(mctrk.PdgCode() < 10000) { 
-        std::cout << "mctrack " << mctrk.PdgCode() << "\t ID : " << mctrk.TrackID() <<  "\t Process : " << mctrk.Process() << " \t Mother ID : " << mctrk.MotherTrackID() <<  " \t Track length : " << length(mctrk)<< std::endl;
-        if(mctrk.size() !=0) {
-          std::cout << " \t \t\t \t Start: [" << mctrk.front().X() << ", " << mctrk.front().Y() << ", " << mctrk.front().Z() << "] \t End: [" << mctrk.back().X() << ", " << mctrk.back().Y() << ", " << mctrk.back().Z() << "] " << std::endl;
-        }     
+    // For mctracks in the event
+    if(mctrk_v->size() != 0) {
+  
+      for(auto mctrk : *mctrk_v)  {
+        if(mctrk.Process() == "primary" && mctrk.PdgCode() == 211 && mctrk.size() != 0) {
+          PionLength->Fill(length(mctrk));
+          PionTrackID = mctrk.TrackID();
+        
+          borderdist[0] = fabs(mctrk.back().X());
+          borderdist[1] = fabs(mctrk.back().X() - 2.*(larutil::Geometry::GetME()->DetHalfWidth()));
+          borderdist[2] = fabs(mctrk.back().Y() - larutil::Geometry::GetME()->DetHalfHeight());
+          borderdist[3] = fabs(mctrk.back().Y() + larutil::Geometry::GetME()->DetHalfHeight());
+          borderdist[4] = fabs(mctrk.back().Z());
+          borderdist[5] = fabs(mctrk.back().Z() - larutil::Geometry::GetME()->DetLength());
+ 	}
+        if(mctrk_v->size() != 0 && mctrk.MotherTrackID() == PionTrackID) countDaughters++;
       }
 
-      if(mctrk.Process() == "primary" && mctrk.PdgCode() == 211) {
-        PionLength->Fill(length(mctrk));
-        PionTrackID = mctrk.TrackID();
+      for(auto mctrk : *mctrk_v) {
+ 	if(mctrk.Process() != "primary" && mctrk.MotherTrackID() == PionTrackID) {
+          pionProcess = mctrk.Process();
+        }
       }
+      // Interaction Modes
+      for(auto mctrk : *mctrk_v) {
+	if(mctrk.Process() == "Decay" && mctrk.MotherTrackID() == PionTrackID ) 	decayPion = true;
+        if(mctrk.Process() == "pi+Inelastic" && mctrk.MotherTrackID() == PionTrackID ) 	piInelasticPion = true;
+	if(minimumValue(borderdist) < 10. && countDaughters == 1)			exitPion = true;
+      }
+
     }
-  }
+
+    if(!decayPion && !piInelasticPion) {
+      MinEndPointDist->Fill(minimumValue(borderdist));
+     // std::cout << "--------Process: " << pionProcess << std::endl; 
+    }
+
+    if(exitPion) {
+      ExitPionMom->Fill(PionMom);
+      ExitPionEnergy->Fill(PionE);
+    }
+
+    if(decayPion) {
+      DecayPionEnergy->Fill(PionE);
+      DecayPionMom->Fill(PionMom);
+    }
+    if(piInelasticPion) {
+      PiInelasticPionEnergy->Fill(PionE);
+      PiInelasticPionMom->Fill(PionMom);
+    }
+
+
+    // get the tracks associated with PFParticles  
+
+    for(auto pfpart : *pfp_v) {
+      // std::cout << "Pandora Particle " << pfpart.PdgCode() << std::endl;
+    }
   
-  for(auto mctrk : *mctrk_v) {
-    if(mctrk.Process() == "Decay" && mctrk.MotherTrackID() == PionTrackID ) {
-      std::cout << "\t---------------->DECAY PION \t decays to \t " << mctrk.PdgCode() << std::endl;
-      decayPion = true;
+    for(size_t pfp_i = 0; pfp_i < ass_vertex.size(); ++pfp_i) { 
+      auto const& pfpp = (*pfp_v)[pfp_i];
+     // std::cout << "  PFParticle @ index = " << pfp_i << " ... ID = " << pfpp.Self() << " ... pdgCode " << pfpp.PdgCode() << " ... # daughters " << pfpp.NumDaughters() << std::endl; 
     }
-    if(mctrk.Process() == "pi+Inelastic" && mctrk.MotherTrackID() == PionTrackID ) {
-      std::cout << "\t ----------------> Pi+ Inelastic Scattering " << std::endl;
-      piInelasticPion = true;
-    }    
+
+    // print information desired
+
+    //decide which events
+    if(!exitPion && decayPion && !piInelasticPion) {
+      // Event info
+      std::cout << "\n----------------------------------------" << std::endl;
+      std::cout << "Run   " << storage->run_id() << "  Event " << storage->event_id() << std::endl << std::endl;
+
+      // Generator level info
+      for(auto par : mcparticles) {
+        std::cout << "MCPART " << par.StatusCode() << "\t PDG : " << par.PdgCode() <<  std::endl;
+      }
+
+      // Geant level info
+      if(mctrk_v->size() != 0) {
+        for(auto mctrk : *mctrk_v)  {
+          if(mctrk.PdgCode() > 0) {
+            std::cout << "mctrack " << mctrk.PdgCode() << "\t ID : " << mctrk.TrackID() <<  "\t Process : " << mctrk.Process() << " \t Mother ID : " << mctrk.MotherTrackID() <<  " \t Track length : " << length(mctrk)<< std::endl;
+            if(mctrk.size() !=0) {
+              std::cout << " \t \t\t \t Start in TPC: [" << mctrk.front().X() << ", " << mctrk.front().Y() << ", " << mctrk.front().Z()  << "] \t End in TPC: [" << mctrk.back().X() << ", " << mctrk.back().Y() << ", " << mctrk.back().Z() << "] " << std::endl;
+            }
+          }
+        }
+      }
+
+      if(decayPion) 	std::cout << "\t---------------->DECAY \t Process : " << pionProcess		<< std::endl;
+      if(piInelasticPion) std::cout << "\t ---------------->Pi+INELASTIC \t Process : "	<< pionProcess 	<<  std::endl;
+      if(exitPion) 	std::cout << "\t ---------------->Pion EXITS TPC \t Process : " << pionProcess 	<< std::endl;
+      if(!decayPion && !piInelasticPion && !exitPion) std::cout << "\t ----------------> Process : " 	<< pionProcess << std::endl;
+    
+
+    // Reconstruction info
+    std::cout << "\n Reconstruction Info " << std::endl;
+
+    for(auto pfpart : *pfp_v) {
+     // std::cout << "Pandora Particle " << pfpart.PdgCode() << std::endl;
+      }
+    for(size_t pfp_i = 0; pfp_i < ass_vertex.size(); ++pfp_i) {
+      auto const& pfpp = (*pfp_v)[pfp_i];
+      std::cout << "  PFParticle @ index = " << pfp_i << " ... ID = " << pfpp.Self() << " ... pdgCode " << pfpp.PdgCode() << " ... is primary? " << pfpp.IsPrimary() << " ... # daughters " << pfpp.NumDaughters() << std::endl; 
+    }
   }
 
-  for(auto mctrk : *mctrk_v) {
-    if(mctrk_v->size() != 0 && mctrk.MotherTrackID() == PionTrackID) countDaughters++;
+    return true;
   } 
-
-  if(mctrk_v->size() != 0 && mctrk_v->at(0).PdgCode() == 211 && mctrk_v->at(0).size() != 0) {
-    std::cout << "End point : " << mctrk_v->at(0).back().X() << ", " << mctrk_v->at(0).back().Y() << ", " << mctrk_v->at(0).back().Z() << std::endl;
-    EndPoint->Fill(mctrk_v->at(0).back().X(), mctrk_v->at(0).back().Y(), mctrk_v->at(0).back().Z());
-    borderdist[0] = fabs(mctrk_v->at(0).back().X());
-    //borderdist[0] = minX1;
-    borderdist[1] = fabs(mctrk_v->at(0).back().X() - 2.*(larutil::Geometry::GetME()->DetHalfWidth()));
-    //borderdist[1] = minX2;
-    borderdist[2] = fabs(mctrk_v->at(0).back().Y() - larutil::Geometry::GetME()->DetHalfHeight());
-    //borderdist[2] = minY1;
-    borderdist[3] = fabs(mctrk_v->at(0).back().Y() + larutil::Geometry::GetME()->DetHalfHeight());
-    //borderdist[3] = minY2;
-    borderdist[4] = fabs(mctrk_v->at(0).back().Z());
-    //borderdist[4] = minZ1;
-    borderdist[5] = fabs(mctrk_v->at(0).back().Z() - larutil::Geometry::GetME()->DetLength());
-    //borderdist[5] = minZ2;
-    std::cout << "************** " << borderdist[0] << ", " <<borderdist[1] << ", " <<borderdist[2] << ", " <<borderdist[3] << ", " <<borderdist[4] << ", " <<borderdist[5]  << std::endl;
-    std::cout << "************* MIN " << minimumValue(borderdist) << std::endl;
-    if(minimumValue(borderdist) < 10. && countDaughters == 1) exitPion = true;
-  }
-
-  if(exitPion) {
-    std::cout << "\n \t-----------------> neither Decay nor pi+inelastic, exits TPC " << std::endl;
-    MinEndPointDist->Fill(minimumValue(borderdist));
-    ExitPionMom->Fill(PionMom);
-    ExitPionEnergy->Fill(PionE);
-  }
-
-  if(!decayPion && !piInelasticPion && !exitPion){
-    std::cout << "!!!***!!! non decay, non inelasitc, non-exit " <<std::endl;
-  }      
-
-  if(decayPion) {
-    DecayPionEnergy->Fill(PionE);
-    DecayPionMom->Fill(PionMom);
-  }
-  if(piInelasticPion) {
-    PiInelasticPionEnergy->Fill(PionE);
-    PiInelasticPionMom->Fill(PionMom);
-  }
-
-  // get the tracks associated with PFParticles  
-
-  for(auto pfpart : *pfp_v) {
-    // std::cout << "Pandora Particle " << pfpart.PdgCode() << std::endl;
-  }
-  
-  for(size_t pfp_i = 0; pfp_i < ass_vertex.size(); ++pfp_i) { 
-    auto const& pfpp = (*pfp_v)[pfp_i];
-    std::cout << "  PFParticle @ index = " << pfp_i << " ... ID = " << pfpp.Self() << " ... pdgCode " << pfpp.PdgCode() << " ... # daughters " << pfpp.NumDaughters() << std::endl; 
-  }
-
-  return true;
-} 
 //----------------------------------------------------
   bool SinglePions::finalize() {
 
